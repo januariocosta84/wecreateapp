5469ca57ab019910b713726c331e9c5196f345ca
januario da costa <jnrdacosta@yahoo.com>
2018-05-23 22:16:24 +0900

    make it better

--- trackingsystem/urls.py
+++ trackingsystem/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path,re_path, include
+from django.urls import path,re_path,url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
-    path('inventory/',include('inventory.urls'), name='inventory'),
+    url('^admin/', admin.site.urls, name='admin'),
+    url('^inventory/',include('inventory.urls'), name='inventory'),
 ]
