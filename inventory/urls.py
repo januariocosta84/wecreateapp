0bc289fef1e4328779614348a7775dd347e7262c
januario da costa <jnrdacosta@yahoo.com>
2018-05-23 22:18:36 +0900

    make it better

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import url,path
 from .import views
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('produto/',views.produto, name='produto'),
-    path('employer/',views.employerlist, name='employer'),
-    path('staff/',views.employersearch,name='employersearch'),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url('',views.index, name='index'),
+    url('produto/',views.produto, name='produto'),
+    url('employer/',views.employerlist, name='employer'),
+    url('staff/',views.employersearch,name='employersearch'),
+    url('login/', auth_views.login, name='login'),
+    url('logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
 
     
 ]
