66eaedde75062f3037ddeb3f1f2f34a00b2e6507
Januario <jnrdacosta@yahoo.com>
2018-05-24 17:19:00 +0900

    make it better

--- trackingsystem/settings.py
+++ trackingsystem/settings.py
@@ -93,7 +93,7 @@ LOGIN_REDIRECT_URL = 'produto'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
