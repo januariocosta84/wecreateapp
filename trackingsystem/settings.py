0e06a0b1ce09832ef013384d207e82c9baa93432
Januario <jnrdacosta@yahoo.com>
2018-05-25 16:37:14 +0900

    make it better

--- trackingsystem/settings.py
+++ trackingsystem/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 #STATIC_PATH=os.path.join(PROJECT_PATH,'static')
