914eb07acd0896f61402caea80a9d752d5d68f26
januario da costa <jnrdacosta@yahoo.com>
2018-06-10 02:23:40 +0900

    make it better

--- trackingsystem/urls.py
+++ trackingsystem/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
