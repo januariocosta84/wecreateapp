d02334233f107b8a47048e5e37181b9c8d3dcee8
januario da costa <jnrdacosta@yahoo.com>
2018-06-10 02:10:37 +0900

    make it better

--- trackingsystem/urls.py
+++ trackingsystem/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     url('^admin/', admin.site.urls, name='admin'),
     url('',include('inventory.urls'), name='inventory'),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
